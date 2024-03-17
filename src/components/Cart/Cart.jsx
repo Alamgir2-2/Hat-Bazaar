cd2e691489ef837d5c6afca9224332a84342d24d
Alamgir2-2 <darkthehunter22@gmail.com>
2024-03-17 16:05:34 +0600

    Cart Updated

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,14 +1,31 @@
 import React from "react";
+import './Cart.css';
 
 const Cart = ({cart}) => {
 
   // const cart = props.cart; ...1
   // const {cart} = props; .... 2
 
+  console.log(cart);
+
+  let total = 0;
+  let totalShipping = 0;
+  for(const product of cart){
+    total = total + product.price;
+    totalShipping = totalShipping + product.shipping;
+  } 
+
+  const tax = total*7/100;
+  const grandTotal = total + totalShipping + tax;
+
   return (
-    <div>
+    <div className="cart">
       <h4>Order Summery</h4>
       <p>Selected Items: {cart.length}</p>
+      <p>Total Prce: ${total}</p>
+      <p>Total Shipping Price: ${totalShipping}</p>
+      <p>Tax: ${tax.toFixed(2)}</p>
+      <h6>Grand Total: ${grandTotal.toFixed(2)}</h6>
     </div>
   );
 };
