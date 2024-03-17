cd9ccf8535884af86432e9ae675bc0bc1c7f6fe3
Alamgir2-2 <darkthehunter22@gmail.com>
2024-03-18 01:46:56 +0600

    Show Data from Cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-import './Cart.css';
+import "./Cart.css";
 
-const Cart = ({cart}) => {
-
+const Cart = ({ cart }) => {
   // const cart = props.cart; ...1
   // const {cart} = props; .... 2
 
@@ -10,18 +9,25 @@ const Cart = ({cart}) => {
 
   let total = 0;
   let totalShipping = 0;
-  for(const product of cart){
-    total = total + product.price;
-    totalShipping = totalShipping + product.shipping;
-  } 
+  let quantity = 0;
+  for (const product of cart) {
+    // if (product.quantity === 0) {
+    //   product.quantity = 1;
+    // }
+    // product.quantity = product.quantity || 1;
 
-  const tax = total*7/100;
+    total = total + product.price * product.quantity;
+    totalShipping = totalShipping + product.shipping;
+    quantity = quantity + product.quantity;
+  }
+
+  const tax = (total * 7) / 100;
   const grandTotal = total + totalShipping + tax;
 
   return (
     <div className="cart">
       <h4>Order Summery</h4>
-      <p>Selected Items: {cart.length}</p>
+      <p>Selected Items: {quantity}</p>
       <p>Total Prce: ${total}</p>
       <p>Total Shipping Price: ${totalShipping}</p>
       <p>Tax: ${tax.toFixed(2)}</p>
