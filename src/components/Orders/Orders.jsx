b0abf7392a038294ad5dba9fd1e98099b3061128
Alamgir2-2 <darkthehunter22@gmail.com>
2024-03-19 00:52:03 +0600

    Updated

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import Cart from '../Cart/Cart';
-import { useLoaderData } from 'react-router-dom';
+import React from "react";
+import Cart from "../Cart/Cart";
+import { useLoaderData } from "react-router-dom";
 
 const Orders = () => {
-  const products = useLoaderData
+  const products = useLoaderData();
+  console.log(products);
   return (
-    <div className='shop-container'>
-      <div className='products-container'>
-        <h2>Order Page</h2>
+    <div className="shop-container">
+      <div className="products-container">
+        <h2>Products: {products.length}</h2>
       </div>
-      <div className='cart-container'>
-        <Cart cart={[]} ></Cart>
+      <div className="cart-container">
+        <Cart cart={[]}></Cart>
       </div>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import Home from "./components/Layout/Home.jsx";
 import Orders from "./components/Orders/Orders.jsx";
 import Inventory from "./components/Inventory/Inventory.jsx";
 import Login from "./components/Login/Login.jsx";
+import cartProductsLoader from "./loaders/cartProductsLoader.js";
 
 const router = createBrowserRouter([
   {
@@ -21,7 +22,8 @@ const router = createBrowserRouter([
       {
         path: 'orders',
         element: <Orders></Orders>,
-        // loader: cartProductsLoader
+        // loader: () => fetch('products.json')
+        loader: cartProductsLoader
       },
       {
         path: 'inventory',
